3bf47da498e3b0e5ec2dadde7cb58ae623350ba8
unknown <cksgh1539@naver.com>
2018-03-27 17:15:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/b10626/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.b10626.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText usernameEt, PasswordEt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEt = (EditText)findViewById(R.id.userID);
+        PasswordEt = (EditText)findViewById(R.id.userPW);
+    }
+
+    public void OnLogin(View view) {
+        String username = usernameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
+        String type = "login";
+
+        BackgroundWork backgroundWork = new BackgroundWork((this));
+        backgroundWork.execute(type, username, password);
+    }
+}
